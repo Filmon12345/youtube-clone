f0dcee6af0c9aaff0b5159aca78dc8a12cad0472
Philodev <zimilifila3676@gmail.com>
2024-01-08 22:56:50 -0800

    correct common mistakes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar =document.querySelector(".sidebar");
 var container = document.querySelector(".container1");
 
-menuIcon.onclick = function(){
+menuIcon.onclick = ()=>{
 
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
